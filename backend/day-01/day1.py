01741b1de9f1efeb1ccec1c05d9047544f862255
Rasty Espartero <gemrasty@gmail.com>
2023-11-21 23:03:09 +0800

    commited files

---
+++ backend/day-01/day1.py
@@ -0,0 +1,6 @@
+name = "Rasty"
+age = 20
+isMale = True
+department = "Department of Software and Web Development"
+
+print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
---
+++ backend/day-02/day-2.py
@@ -0,0 +1,10 @@
+# Prompt the user to enter three integer values
+value1 = int(input("Enter the first integer: "))
+value2 = int(input("Enter the second integer: "))
+value3 = int(input("Enter the third integer: "))
+
+# Calculate the sum
+sum_result = value1 + value2 + value3
+
+# Display the result
+print(f"The sum of {value1}, {value2}, and {value3} is: {sum_result}")
