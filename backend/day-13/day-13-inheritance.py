1d4d245ceaa40f6615ed0ceae850afc2adf2f5cb
Rasty Espartero <gemrasty@gmail.com>
2024-03-31 14:28:31 +0800

    Update day-13-inheritance.py

--- backend/day-13/day-13-inheritance.py
+++ backend/day-13/day-13-inheritance.py
@@ -1,17 +1,23 @@
 class BasicCalculator:
     def sum(self, numlist: list):
         _sum = 0
-
         for num in numlist:
             _sum += num
-
         return _sum
 
-class ComplexCalculator(BasicCalculator):
-    pass
+class ComplexCalculator(BasicCalculator):  
+    @staticmethod
+    def power(base, exponent):  
+        return base ** exponent
+
+    @staticmethod
+    def abs(number):  
+        if number >= 0:
+            return number
+        return -number
 
 basic = BasicCalculator()
 complex = ComplexCalculator()
 
 print(basic.sum([1, 2, 3]))
-print(complex.sum([1, 2, 3]))
+print(complex.sum([1, 2, 3]))  
