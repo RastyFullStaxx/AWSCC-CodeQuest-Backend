a8738609b2a90b3fd870318846065a51905fb09a
Rasty Espartero <gemrasty@gmail.com>
2024-03-31 14:24:16 +0800

    Update day-12-classes.py

--- backend/day-12/day-12-classes.py
+++ backend/day-12/day-12-classes.py
@@ -1,17 +1,21 @@
 class Student:
-    def __init__(self, name, age, 
-        is_enrolled, classes, offenses):
+    def __init__(self, name, age, is_enrolled, classes, offenses):
         self.name = name
         self.age = age
         self.is_enrolled = is_enrolled
         self.classes = classes
         self.offenses = offenses
-
+        
+    # add a new class to the student
+    def add_class(self, new_class):
+        self.classes.append(new_class)
+        
 student1 = Student("Juan Dela Cruz", 20, True, [], [])
+student1.add_class("Math")
+student1.add_class("Science")
 
-print(f"Name: {student1.name}")
-print(f"Age: {student1.age}")
-print(f"Status: {'Enrolled' if student1.is_enrolled else 'Not Enrolled Yet'}")
-print(f"Classes: {student1.classes}")
-print(f"Offenses: {student1.offenses}")
-
+print("Student:", student1.name)
+print("Age:", student1.age)
+print("Enrolled:", student1.is_enrolled)
+print("Classes:", student1.classes)
+print("Offenses:", student1.offenses)
